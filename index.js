196e5919e2cb7ff845b939d7c46e037ed116fc82
Miguel Aragón <miguel.aragon@base22.com>
2017-02-06 16:31:54 -0600

    Add support for render functions and widget styles

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ hexo.extend.helper.register( "render_widgets", ( html, sidebarEnabled ) => {
 		error: null
 	};
 
-	jsDOM.env({
+	jsDOM.env( {
 		html: html,
-		done: function (error, window) {
+		done: function( error, window ) {
 			if( error ) {
 				result.error = error;
 				return;
@@ -23,15 +23,15 @@ hexo.extend.helper.register( "render_widgets", ( html, sidebarEnabled ) => {
 
 			processDOM( window, sidebarEnabled ).then( () => {
 				result.html = window.document.querySelector( "html" ).outerHTML;
-			}).catch( ( error ) => {
+			} ).catch( ( error ) => {
 				result.error = error;
 				console.error( error );
-			});
+			} );
 		}
-	});
+	} );
 
 	// Hexo doesn't support asynchronous helpers so we need to convert the asynchronous action to a synchronous
-	deAsync.loopWhile(() => ! result.html && ! result.error );
+	deAsync.loopWhile( () => ! result.html && ! result.error );
 
 	return "<!DOCTYPE html>" + result.html;
 } );
@@ -48,27 +48,103 @@ function processDOM( window ) {
 
 	let widgets = glob.sync( hexo.theme.base + "widgets/*.js" ).map( ( file ) => {
 		return require( path.resolve( file ) );
-	});
+	} );
 
 	let widgetPromises = [];
+
 	for( let widget of widgets ) {
 		let elements = $$( widget.selector );
 
-		for (let i = 0; i < elements.length; ++i) {
-			let element = elements[i];
-			let widgetData = {};
-			let widgetPromise = widget.preRender( element, widgetData, document, _ ).then( () => {
-				// TODO: Use readFile instead of readFileSync
-				// TODO: Reject the promise if there is no template or templateURL defined
-				// TODO: Allow templateURL outside of the base (or even local to the widget file)
-				let widgetTemplate = "template" in widget ? widget.template : "templateURL" in widget ? fs.readFileSync( hexo.theme.base + "widgets/" + widget.templateURL ).toString() : "";
+		for( let i = 0; i < elements.length; ++ i ) {
+			let element = elements[ i ];
 
-				element.outerHTML = ejs.render( widgetTemplate, widgetData );
+			let data = {};
+
+			let widgetPromise = preRender( element, widget, data, document, _ ).then( () => {
+				if( "template" in widget || "templateURL" in widget ) return renderWidgetWithTemplate( widget, data, element, document, _ );
+				else if( "render" in widget ) return renderWidget( widget, data, element, document, _ );
+				else return Promise.resolve();
 			} );
 
 			widgetPromises.push( widgetPromise );
 		}
+
+		if( elements.length > 0 ) widgetPromises.push( addWidgetAssets( widget, document, _ ) );
 	}
 
+	removeAssetPlaceholders( document, _ );
+
 	return Promise.all( widgetPromises );
+}
+
+function preRender( widget, data, element, document, _ ) {
+	return "preRender" in widget ? widget.preRender( widget, data, element, document, _ ) : Promise.resolve();
+}
+
+function renderWidgetWithTemplate( widget, data, element, document, _ ) {
+	// TODO: Use readFile instead of readFileSync
+	// TODO: Reject the promise if there is no template or templateURL defined
+	// TODO: Allow templateURL outside of the base (or even local to the widget file)
+	let template = "template" in widget ? widget.template : "templateURL" in widget ? fs.readFileSync( hexo.theme.base + "widgets/" + widget.templateURL ).toString() : "";
+
+	element.outerHTML = ejs.render( template, data );
+
+	return Promise.resolve();
+}
+
+function renderWidget( widget, data, element, document, _ ) {
+	return widget.render( widget, data, element, document, _ );
+}
+
+function addWidgetAssets( widget, document, _ ) {
+	let promises = [];
+
+	if( "styles" in widget ) promises.push( addWidgetStyles( widget, document, _ ) );
+	if( "scripts" in widget ) promises.push( addWidgetScripts( widget, document, _ ) );
+
+	return Promise.all( promises );
+}
+
+function addWidgetStyles( widget, document, _ ) {
+	let styles = widget.styles;
+
+	let promises = [];
+	for( let style of styles ) {
+		promises.push( addWidgetStyle( style, widget, document, _ ) );
+	}
+
+	return Promise.all( promises );
+}
+
+function addWidgetStyle( style, widget, document, _ ) {
+	if( ( "inline" in style && style.inline ) || "source" in style ) {
+		let styleSource = "";
+
+		if( "file" in style ) {
+			styleSource = fs.readFileSync( style.file ).toString();
+		} else if( "source" in style ) {
+			styleSource = style.source;
+		}
+
+		let styleNode = document.createElement( "style" );
+		styleNode.innerHTML = styleSource;
+
+		let $widgetsStyles = _.$( "#widgets-styles" );
+		$widgetsStyles.parentNode.insertBefore( styleNode, $widgetsStyles );
+	} else {
+		// TODO
+	}
+}
+
+function addWidgetScripts( widget, document, _ ) {
+	// TODO
+	return Promise.resolve();
+}
+
+function removeAssetPlaceholders( document, _ ) {
+	let $stylesPlaceholder = _.$( "#widgets-styles" );
+	if( $stylesPlaceholder ) $stylesPlaceholder.remove();
+
+	let $scriptsPlaceholder = _.$( "#widgets-scripts" );
+	if( $scriptsPlaceholder ) $scriptsPlaceholder.remove();
 }
